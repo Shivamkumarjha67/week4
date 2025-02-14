f6ec49a876f84b2d318b6bfd1f9be335fd3143bb
Shivam Kumar Jha <shkrjh@gmail.com>
2025-02-14 16:34:57 +0530

    [Shivam Kumar Jha] Add . Handling the custom exception solution is added

---
+++ day4/src/main/java/CustomException/InvalidAgeException.java
@@ -0,0 +1,10 @@
+package CustomException;
+
+// InvalidAgeException class definition
+public class InvalidAgeException extends Exception {
+    // Parameterized constructor
+    InvalidAgeException(String message) {
+        // Passing message to constructor to exception class constructor
+        super(message);
+    }
+}

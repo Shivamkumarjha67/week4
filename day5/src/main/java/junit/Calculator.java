4d2879b594f74b0cc86718ea058ee2ddbf87edc8
Shivam Kumar Jha <shkrjh@gmail.com>
2025-02-17 13:12:56 +0530

    [Shivam Kumar Jha] Add . Calculator file for testing is added

---
+++ day5/src/main/java/junit/Calculator.java
@@ -0,0 +1,23 @@
+package junit;
+
+public class Calculator {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int subtract(int a , int b ){
+        return a - b;
+    }
+
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
+    public int divide(int a , int b) throws ArithmeticException{
+//        if(a == 0){
+//            throw new ArithmeticException("Division by zero is not allowed");
+//        }
+        return a / b;
+    }
+}

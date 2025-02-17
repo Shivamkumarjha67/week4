738e0a141dac6cbd81e4af26a3e64e47dbd370d7
Shivam Kumar Jha <shkrjh@gmail.com>
2025-02-17 13:14:38 +0530

    [Shivam Kumar Jha] Add . Divide file for testing is added

---
+++ day5/src/main/java/junit/Divide.java
@@ -0,0 +1,10 @@
+package junit;
+
+public class Divide {
+    public static int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
+}

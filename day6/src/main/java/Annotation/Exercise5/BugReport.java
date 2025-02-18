9eae6ea5c4b59154501cd6f1134a30a606c314c0
Shivam Kumar Jha <shkrjh@gmail.com>
2025-02-18 16:14:26 +0530

    [Shivam Kumar Jha] Add . Exercise 5 solution added

---
+++ day6/src/main/java/Annotation/Exercise5/BugReport.java
@@ -0,0 +1,16 @@
+package Annotation.Exercise5;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// Repeatable annotation
+@Repeatable(BugReports.class)  // Link to container annotation
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface BugReport {
+    String description();
+}
+
---
+++ day6/src/main/java/Annotation/Exercise5/Software.java
@@ -0,0 +1,9 @@
+package Annotation.Exercise5;
+
+class Software {
+    @BugReport(description = "Null pointer exception occurs on invalid input.")
+    @BugReport(description = "Performance issue when handling large data.")
+    public void processData() {
+        System.out.println("Processing data...");
+    }
+}

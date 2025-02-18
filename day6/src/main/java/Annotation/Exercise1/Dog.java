fecf8a9b15582fcb7b7ea09c4c6ce71d649ec56e
Shivam Kumar Jha <shkrjh@gmail.com>
2025-02-18 16:09:09 +0530

    [Shivam Kumar Jha] Add . Exercise 1 solution added

---
+++ day6/src/main/java/Annotation/Exercise1/Dog.java
@@ -0,0 +1,9 @@
+package Annotation.Exercise1;
+
+class Dog extends Animal {
+    // Override the makeSound method
+    @Override
+    public void makeSound() {
+        System.out.println("The dog barks");
+    }
+}

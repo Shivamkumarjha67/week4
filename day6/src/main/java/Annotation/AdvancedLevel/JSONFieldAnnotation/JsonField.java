fe627f9f1c08bdf509309754ad363ef8ef8777a7
Shivam Kumar Jha <shkrjh@gmail.com>
2025-02-18 15:54:08 +0530

    [Shivam Kumar Jha] Add . JSON field annotation solution added

---
+++ day6/src/main/java/Annotation/AdvancedLevel/JSONFieldAnnotation/JsonField.java
@@ -0,0 +1,10 @@
+package Annotation.AdvancedLevel.JSONFieldAnnotation;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME) // Annotation available at runtime
+@Target(ElementType.FIELD) // Can be used on fields only
+public @interface JsonField {
+    String name(); // Custom JSON key name
+}
+
